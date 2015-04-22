750110874e2d128b467befb8159e158b9766e1fd
sjefremov <stojanco.jefremov@gmail.com>
2015-04-22 17:03:23 +0200

    BikeRegisterMVC

---
+++ BikeRegister/Register/Register/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Register.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Register.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            //TODO create the model
+            List<BikeModel> _result = new List<BikeModel>();
+            _result.Add(new BikeModel() { Colour = "red", Model = "Specialized", Producer = "R1", RegNumber="123" });
+            _result.Add(new BikeModel() { Colour = "red", Model = "Specialized", Producer = "R1", RegNumber = "123" });
+            _result.Add(new BikeModel() { Colour = "red", Model = "Specialized", Producer = "R1", RegNumber = "123" });
+            _result.Add(new BikeModel() { Colour = "red", Model = "Specialized", Producer = "R1", RegNumber = "123" });
+            return View(_result);
+        }
+
+    }
+}
---
+++ BikeRegister/Register/Register/Models/BikeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Register.Models
+{
+    public class BikeModel
+    {
+        public string RegNumber { get; set; }
+        public string Producer { get; set; }
+        public string Model { get; set; }
+        public string Colour { get; set; }
+    }
+}
