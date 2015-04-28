500f343dc3e84f55c79607f986e76d0aee42ba28
Stojanco Jefremov <stojanco.jefremov@gmail.com>
2015-04-28 10:34:02 +0200

    RegistarApp + Nlog + Ninject

--- Homework/Registar/ConsoleApplication1/Program.cs
+++ Homework/Registar/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ using Registar.BusinessLayer;
 using Registar.Repository;
 using Registar.Factory;
 using NLog;
+using Ninject;
+using System.Reflection;
 
 namespace ConsoleApplication1
 {
@@ -40,30 +42,36 @@ namespace ConsoleApplication1
             /*
     * Welcome to this NLog demo
     */
-            Console.Out.WriteLine("Greetings, some loggings is about to take place.");
-            Console.Out.WriteLine("");
+            //Console.Out.WriteLine("Greetings, some loggings is about to take place.");
+            //Console.Out.WriteLine("");
 
-            /*
-             * This creates your logger. 
-             * Notice that in the config file we defined that everything should go into the file.
-             * (and if you set the console, it'll output to it as well).
-             */
-            Logger logger = LogManager.GetCurrentClassLogger();
+            ///*
+            // * This creates your logger. 
+            // * Notice that in the config file we defined that everything should go into the file.
+            // * (and if you set the console, it'll output to it as well).
+            // */
+            //Logger logger = LogManager.GetCurrentClassLogger();
 
-            Console.Out.WriteLine("Let's assume you're going to work, and using the bus to get there:");
-            Console.Out.WriteLine("------------------------------------------------------------------");
-            logger.Trace("Trace: The chatter of people on the street");
-            logger.Debug("Debug: Where are you going and why?");
-            logger.Info("Info: What bus station you're at.");
-            logger.Warn("Warn: You're playing on the phone and not looking up for your bus");
-            logger.Error("Error: You get on the wrong bus.");
-            logger.Fatal("Fatal: You are run over by the bus.");
+            //Console.Out.WriteLine("Let's assume you're going to work, and using the bus to get there:");
+            //Console.Out.WriteLine("------------------------------------------------------------------");
+            //logger.Trace("Trace: The chatter of people on the street");
+            //logger.Debug("Debug: Where are you going and why?");
+            //logger.Info("Info: What bus station you're at.");
+            //logger.Warn("Warn: You're playing on the phone and not looking up for your bus");
+            //logger.Error("Error: You get on the wrong bus.");
+            //logger.Fatal("Fatal: You are run over by the bus.");
 
-            /*
-             * Closing app
-             */
-            Console.Out.WriteLine("");
-            Console.Out.WriteLine("Done logging.");
+            ///*
+            // * Closing app
+            // */
+            //Console.Out.WriteLine("");
+            //Console.Out.WriteLine("Done logging.");
+
+            Ninject.IKernel kernal = new StandardKernel();
+            kernal.Bind<IWeapon>().To<Ninja>();
+            var instance = kernal.Get<sourav>();
+            instance.Attack();
+            Console.ReadLine(); 
             Console.Out.WriteLine("Hit any key to exit");
             Console.ReadKey();
         }
